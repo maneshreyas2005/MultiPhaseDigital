f8dcdbc70c6cf77c937a01697cb58838c659af2e
maneshreyas2005 <shreyasmane100@gmail.com>
2025-09-02 19:12:16 +0530

    added robots.txt file and sitemap

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePluginSitemap } from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
-  
+  VitePluginSitemap({
+    hostname: 'https://multiphasedigital.com',
+    routes: [
+      '/',
+      '/#homePage',
+      '/#Services',
+      '/#Testimonial',
+      '/#OurPresence',
+      '/#OurTeam',
+      '/ContactUs'
+    ],
+  }),
   ],
+
 })
