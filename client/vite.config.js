ba8fc0e6b44229bf78ac4620bc74663143dcaa53
maneshreyas2005 <shreyasmane100@gmail.com>
2025-09-11 13:17:42 +0530

    added favicons and formatted code2

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     vitePluginSitemap({
       hostname: "https://multiphasedigital.com",
-      generateRobotsTxt: true,
+      generateRobotsTxt: false,
       routes: [
         "/",
         "/#homePage",
